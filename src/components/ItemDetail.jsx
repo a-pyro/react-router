0fac0fce666ccff68d9363c02cd96cb03c475ce5
ardigermenji <ardi.germenji@gmail.com>
2021-04-03 13:30:18 +0200

    adds loading state to item detail

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 
-const ItemDetail = (props) => {
+const ItemDetail = ({ match: { params } }) => {
   const [item, setItem] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchItem();
@@ -9,13 +10,28 @@ const ItemDetail = (props) => {
 
   const fetchItem = async () => {
     const resp = await fetch(
-      `https://fakestoreapi.com/products/${props.match.params.itemId}`
+      `https://fakestoreapi.com/products/${params.itemId}`
     );
     const data = await resp.json();
     console.log(data);
     setItem(data);
+    setIsLoading(false);
   };
-  return <img src={item.image} />;
+  // return isLoading ? (
+  //   <h1>Loading...</h1>
+  // ) : (
+  // <div>
+  //   <img style={{ width: '200px' }} src={item.image} alt='product' />
+  // </div>
+  // );
+
+  return (
+    (isLoading && <h1>Loading</h1>) || (
+      <div>
+        <img style={{ width: '200px' }} src={item.image} alt='product' />
+      </div>
+    )
+  );
 };
 
 export default ItemDetail;
